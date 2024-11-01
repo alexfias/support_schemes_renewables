8962645cc9aad05fdf97e0ea26b8d11efe1111e7
Alexander Kies <kies@fias.uni-frankfurt.de>
2024-11-01 23:49:40 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,7 @@ def apply_co2_price(network, co2_price):
         if 'co2' in carrier:
             network.carriers.at[carrier, 'co2_emissions'] *= co2_price
 
+
 def update_network_with_scenario(network, scenario_config):
     """
     Update the network with scenario-specific parameters.
@@ -22,3 +23,17 @@ def update_network_with_scenario(network, scenario_config):
     """
     if 'co2_price' in scenario_config:
         apply_co2_price(network, scenario_config['co2_price'])
+
+    if 'storage_cost' in scenario_config:
+        update_storage_cost(network, scenario_config['storage_cost'])
+    
+    if 'fossil_prices' in scenario_config:
+        update_fossil_prices(network, scenario_config['fossil_prices'])
+
+    if weather_year in scenario_config:
+        update_weather_year(network, scenario_config['weather_year'])
+
+    if transmission_expansion in scenario_config:
+        update_transmission_expansion(network, scenario_config['transmission_expansion])
+        
+    return network
